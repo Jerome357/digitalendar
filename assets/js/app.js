9d13dc956b7ad185c2412cefc1010c3e51ef7f08
Jerome Duval <jerome.duval35@hotmail.fr>
2019-07-01 15:12:18 +0200

    Ajout de twig et séparation du body

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,14 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../css/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+
+const $ = require('jquery');
+require("popper.js");
+require("bootstrap");
+require("select2");
+require("tinymce");
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
